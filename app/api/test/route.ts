49a6ee5c8375aa2463ff0b930fc0be1f310093c9
Linus1Kn <linus1knuts@gmail.com>
2025-12-09 12:39:03 +0200

    test

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,3 +1,17 @@
+const messages:[any?] = [{
+    name: "Initial example Name",
+    message: "Test Msg2",
+}]
+
 export async function GET() {
-    return Response.json({message: "Test"})
+    return Response.json(messages)
+}
+
+export async function POST(what:any) {
+    console.log("from client ", what.name)
+    messages.push({
+        name: "Test Name",
+        message: "Test Msg",
+    })
+    return Response.json(messages)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,24 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
 
-  const [data, setData] = useState(null)
+  const [result, setResult] = useState(null)
 
   async function getData() {
     const res = await fetch("/api/test")
     const data = await res.json()
     console.log(data)
-    setData(data)
+    setResult(data)
+  }
+
+  async function postData(e: React.FormEvent) {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    console.log("sent", data)
+    const res = await fetch('/api/test', { method: 'POST', body: JSON.stringify(data) })
+    const resdata = await res.json()
+    console.log("res", resdata)
   }
 
   useEffect(() => {
@@ -21,11 +32,30 @@ export default function Home() {
   }, [])
   
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-4xl">
-        api message: 
-        <span className={data ? "text-green-400" : "text-amber-400"}>{data ? data.message : "LOADING"}</span>
-      </h1>
+    <div className="flex flex-col min-h-screen items-center justify-center gap-2 bg-zinc-50 font-sans dark:bg-black">
+      <form onSubmit={postData} className="flex flex-col">
+        <label htmlFor="name">Name</label>
+        <input required type="text" name="name" id="name" placeholder="Name here" />
+
+        <label htmlFor="message">Message</label>
+        <textarea required name="message" id="message" placeholder="Message here"></textarea>
+
+        <button type="submit" className="p-2 bg-green-500 hover:bg-green-400 rounded-md">Publish</button>
+      </form>
+
+      <div className="w-full h-0.5 bg-gray-500 max-w-1/2"></div>
+
+      <div>
+
+        {result === null && <p>Loading...</p>}
+        {Array.isArray(result) &&
+          result.map((item, idx) => (
+            <p key={idx}>
+              <strong>{item.name}:</strong> {item.message}
+            </p>
+        ))}
+
+      </div>
     </div>
   );
 }
